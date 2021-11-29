e3088c7a41c3481e797bed75ef62f67e0fd47fd2
Nicolas Chaves <nicolas.chaves93@gmail.com>
2021-11-29 02:15:21 -0500

    Agregar Enlaces Clientes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'
-import Reportes from './pages/Reportes'
-import Productos from './pages/Productos'
-import Clientes from "./pages/Clientes";
 import React from "react";
+
+import Home from './pages/Home'
+
+import Productos from './pages/Productos'
 import AgregarProducto from './components/AgregarProducto'
 import EditarProducto from "./components/EditarProducto";
+
+import Clientes from "./pages/Clientes";
+import AgregarCliente from "./components/AgregarCliente";
+import EditarCliente from "./components/EditarCliente";
+
 import Proveedores from "./pages/Proveedores"
 import AgregarProveedor from "./components/AgregarProveedor"
 import EditarProveedor from "./components/EditarProveedor"
@@ -20,11 +25,15 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path='/' element={<Home/>}/>
+          {/* Enlaces Clientes */}
           <Route exact path='/clientes' element={<Clientes/>}/>
-          <Route exact path='/reportes' element={<Reportes/>}/>
+          <Route exact path='/agregarCliente' element={<AgregarCliente/>}/>
+          <Route exact path='/editarCliente/:id' element={<EditarCliente/>}/>
+          {/* Enlaces Productos */}
           <Route exact path='/productos' element={<Productos/>}/>
           <Route exact path='/agregarProducto' element={<AgregarProducto/>}/>
           <Route exact path='/editarProducto/:id' element={<EditarProducto/>}/>
+          {/* Enlaces Proveedores */}
           <Route exact path='/proveedores' element={<Proveedores/>}/>
           <Route exact path='/agregarProveedor' element={<AgregarProveedor/>}/>
           <Route exact path='/editarProveedor/:id' element={<EditarProveedor/>}/>
